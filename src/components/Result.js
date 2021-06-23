8de4a74e77898d70661a546bc3d1c42d190018a3
Consecratio <jcasique@gmail.com>
2021-06-23 14:34:48 -0700

    added alt tag to images

--- src/components/Result.js
+++ src/components/Result.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid'
 export default class Result extends Component {
     render() {
         let searchResults = this.props.results.map((result) => {
-            return <li key={ uuidv4() }><img src={result} /></li>
+            return <li key={ uuidv4() }><img src={result} alt="" /></li>
         })
 
         return (
